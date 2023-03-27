425a831066fbe66f85d22bd0d41200935169e13c
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-03-27 11:28:21 -0300

    update

---
+++ Arquivos/script.js
@@ -0,0 +1,141 @@
+// EP IV
+function ocultarIV() {
+    let msg = document.querySelector('#divIV').innerHTML = "<h3>Star Wars: Episódio IV – Uma Nova Esperança</h3>"
+}
+function mostrarIV() {
+    let msg = document.querySelector('#divIV').innerHTML = "<h3>Star Wars: Episódio IV – Uma Nova Esperança</h3>"
+
+    let msg2 = document.querySelector('#divIV').innerHTML += "<p>O primeiro da saga homônima a ser lançado em 25 de maio de 1977, e o quarto na ordem cronológica da trama, escrito e dirigido por George Lucas. Conta a história do jovem fazendeiro Luke Skywalker (Mark Hamill), que, após adquirir dois droids (robôs), envolve-se na Guerra Civil Galáctica entre o tirano Império Galáctico e a Aliança Rebelde, ao lado do contrabandista Han Solo (Harrison Ford). Um dos robôs contém na memória o projeto secreto da super arma do Império, Estrela da Morte, roubado pelos rebeldes e inserido no droid por sua líder, Princesa Leia (Carrie Fisher), que com isso espera destruir o Império e salvar a galáxia.</p>"
+    
+    let img = document.querySelector('#divIV').innerHTML += "<img src='./imagens/epIV.jpg'>"
+}
+
+
+// EP V
+function ocultarV() {
+    let msg = document.querySelector('#divV').innerHTML = "<h3>Star Wars: Episódio V – O Império Contra-Ataca</h3>"
+}
+function mostrarV() {
+    let msg = document.querySelector('#divV').innerHTML = "<h3>Star Wars: Episódio V – O Império Contra-Ataca</h3>"
+
+    let msg2 = document.querySelector('#divV').innerHTML += "<p>O filme é ambientado três anos depois de Guerra nas Estrelas. O Império Galáctico, sob a liderança do vilão Darth Vader e do misterioso Imperador, está em busca de Luke Skywalker e o resto da Aliança Rebelde. Enquanto Vader procura incansavelmente o pequeno grupo de amigos de Luke — Han Solo, Princesa Leia Organa e outros — através da galáxia, Luke é ensinado pelo Mestre Yoda a como usar a Força. Quando Vader captura os amigos de Luke, Luke deve decidir se vai completar seu treinamento e se tornar um Cavaleiro Jedi completo ou confrontar Vader e salvá-los.</p>"
+    
+    let img = document.querySelector('#divV').innerHTML += "<img src='./imagens/epV.jpg'>"
+}
+
+
+// EP VI
+function ocultarVI() {
+    let msg = document.querySelector('#divVI').innerHTML = "<h3>Star Wars: Episode VI – Return of the Jedi</h3>"
+}
+function mostrarVI() {
+    let msg = document.querySelector('#divVI').innerHTML = "<h3>Star Wars: Episode VI – Return of the Jedi</h3>"
+
+    let msg2 = document.querySelector('#divVI').innerHTML += "<p>A história se passa aproximadamente um ano depois dos eventos de Star Wars Episode V: The Empire Strikes Back.[8] O Império Galático, sob a supervisão militar do impiedoso Darth Vader, começou a construção de uma segunda Estrela da Morte, visando aniquilar a Aliança Rebelde. Como o Imperador Palpatine, também conhecido como Darth Sidious, planeja supervisionar pessoalmente as etapas finais de sua construção, a Frota Rebelde dá início a um ataque em grande escala à Estrela da Morte, para evitar o término de sua construção e matar Palpatine, pondo efetivamente um fim ao Império. Neste meio tempo, Luke Skywalker, um líder rebelde e cavaleiro jedi, luta para resgatar Vader, um ex-jedi que também é seu pai, de volta do Lado Sombrio da Força.</p>"
+    
+    let img = document.querySelector('#divVI').innerHTML += "<img src='./imagens/epVI.jpg'>"
+}
+
+
+// EP I
+function ocultarI() {
+    let msg = document.querySelector('#divI').innerHTML = "<h3>Star Wars: Episódio I – A Ameaça Fantasma</h3>"
+}
+function mostrarI() {
+    let msg = document.querySelector('#divI').innerHTML = "<h3>Star Wars: Episódio I – A Ameaça Fantasma</h3>"
+
+    let msg2 = document.querySelector('#divI').innerHTML += "<p>Ameaça Fantasma ocorre cerca de 32 anos antes do filme original, e segue o Mestre Jedi Qui-Gon Jinn e seu padawan Obi-Wan Kenobi, como protetores da Rainha Amidala, na esperança de conseguir um fim pacífico para uma disputa comercial interplanetária de grande escala. Em sua jornada juntam-se a eles Anakin Skywalker—um jovem escravo com forte conexão com a Força—simultaneamente com o retorno dos misteriosos Sith.</p>"
+    
+    let img = document.querySelector('#divI').innerHTML += "<img src='./imagens/epI.jpg'>"
+}
+
+
+// EP II
+function ocultarII() {
+    let msg = document.querySelector('#divII').innerHTML = "<h3>Star Wars: Episódio II – Ataque dos Clones</h3>"
+}
+function mostrarII() {
+    let msg = document.querySelector('#divII').innerHTML = "<h3>Star Wars: Episódio II – Ataque dos Clones</h3>"
+
+    let msg2 = document.querySelector('#divII').innerHTML += "<p>O filme ocorre 10 anos após os acontecimentos de Star Wars Episódio I: A Ameaça Fantasma. A galáxia está à beira de uma guerra civil. Liderado por um ex-Jedi chamado Conde Dooku, milhares de sistemas planetários ameaçam deixar a República Galáctica. Após a senadora Padmé Amidala escapar de uma tentativa de assassinato, Anakin Skywalker, um aprendiz Jedi, se torna seu protetor, enquanto seu mestre Obi-Wan Kenobi, investiga o atentado contra a vida dela. Logo Anakin, Padmé, e Obi-Wan testemunham o início de uma nova ameaça à galáxia, as Guerras Clônicas.</p>"
+    
+    let img = document.querySelector('#divII').innerHTML += "<img src='./imagens/epII.jpg'>"
+}
+
+
+// EP III
+function ocultarIII() {
+    let msg = document.querySelector('#divIII').innerHTML = "<h3>Star Wars: Episódio III – A Vingança dos Sith</h3>"
+}
+function mostrarIII() {
+    let msg = document.querySelector('#divIII').innerHTML = "<h3>Star Wars: Episódio III – A Vingança dos Sith</h3>"
+
+    let msg2 = document.querySelector('#divIII').innerHTML += "<p>O filme começa três anos após o início das Guerras Clônicas. Os Cavaleiros Jedi estão espalhados por toda a galáxia, liderando uma guerra maciça contra os Separatistas. O Conselho Jedi incumbe o Mestre Jedi Obi-Wan Kenobi de eliminar o notório General Grievous, líder do Exército Separatista. Enquanto isto, o Cavaleiro Jedi Anakin Skywalker se aproxima de Palpatine, o Supremo Chanceler da República Galáctica e, desconhecido do público, um Lord Sith. Sua amizade aprofundada ameaça a Ordem Jedi, a República, e o próprio Anakin.</p>"
+    
+    let img = document.querySelector('#divIII').innerHTML += "<img src='./imagens/epIII.jpg'>"
+}
+
+
+// EP VII
+function ocultarVII() {
+    let msg = document.querySelector('#divVII').innerHTML = "<h3>Star Wars: Episode VII — The Force Awakens</h3>"
+}
+function mostrarVII() {
+    let msg = document.querySelector('#divVII').innerHTML = "<h3>Star Wars: Episode VII — The Force Awakens</h3>"
+
+    let msg2 = document.querySelector('#divVII').innerHTML += "<p>O Despertar da Força se passa aproximadamente trinta anos após os acontecimentos de Return of the Jedi, e acompanha a jornada de Rey, que acaba encontrando um droide, BB-8, com informações do paradeiro de Luke Skywalker que está desaparecido. Ela é acompanhada por Finn, um stormtrooper desertor da Primeira Ordem, uma organização que quer reaver o Império Galáctico e luta contra a Resistência, comandada por Leia Organa. A aventura inesperada leva Rey a descobrir sua forte conexão com a Força e enfrentar o Lado Sombrio.</p>"
+    
+    let img = document.querySelector('#divVII').innerHTML += "<img src='./imagens/epVII.jpg'>"
+}
+
+
+// EP LJ
+function ocultarLJ() {
+    let msg = document.querySelector('#divLJ').innerHTML = "<h3>Star Wars: The Last Jedi</h3>"
+}
+function mostrarLJ() {
+    let msg = document.querySelector('#divLJ').innerHTML = "<h3>Star Wars: The Last Jedi</h3>"
+
+    let msg2 = document.querySelector('#divLJ').innerHTML += "<p>Em Star Wars: Episódio VIII, após encontrar o mítico e recluso Luke Skywalker (Mark Hamill) em uma ilha isolada, a jovem Rey (Daisy Ridley) busca entender o balanço da Força a partir dos ensinamentos do Mestre Jedi. Paralelamente, a Primeira Ordem de Kylo Ren (Adam Driver) se reorganiza para enfrentar a Resistência.</p>"
+    
+    let img = document.querySelector('#divLJ').innerHTML += "<img src='./imagens/epLJ.png'>"
+}
+
+
+// EP RS
+function ocultarRS() {
+    let msg = document.querySelector('#divRS').innerHTML = "<h3>Star Wars: The Rise of Skywalker</h3>"
+}
+function mostrarRS() {
+    let msg = document.querySelector('#divRS').innerHTML = "<h3>Star Wars: The Rise of Skywalker</h3>"
+
+    let msg2 = document.querySelector('#divRS').innerHTML += "<p>Em Star Wars: Episódio IX, com o retorno do Imperador Palpatine (Ian McDiarmid), todos voltam a temer seu poder. Assim, a Resistência toma a frente da batalha que ditará os rumos da galáxia. Treinando para ser uma completa Jedi, Rey (Daisy Ridley) ainda se encontra em conflito com seu passado e futuro, mas teme pelas respostas que pode conseguir a partir de sua complexa ligação com Kylo Ren (Adam Driver), que também se encontra em conflito pela Força.</p>"
+    
+    let img = document.querySelector('#divRS').innerHTML += "<img src='./imagens/epRS.jpg'>"
+}
+
+
+// EP RO
+function ocultarRO() {
+    let msg = document.querySelector('#divRO').innerHTML = "<h3>Rogue One: A Star Wars Story</h3>"
+}
+function mostrarRO() {
+    let msg = document.querySelector('#divRO').innerHTML = "<h3>Rogue One: A Star Wars Story</h3>"
+
+    let msg2 = document.querySelector('#divRO').innerHTML += "<p>Em Rogue One: Uma História Star Wars, Jyn Erso (Felicity Jones) foi afastada de seu pai, Galen (Mads Mikkelsen), ainda criança, devido à exigência do diretor Krennic (Ben Mendelsohn) que ele trabalhasse na construção da arma mais poderosa do Império, a Estrela da Morte. Criada por Saw Gerrera (Forest Whitaker), ela teve que aprender a sobreviver por conta própria ao completar 16 anos. Já adulta, Jyn é resgatada da prisão pela Aliança Rebelde, que deseja ter acesso a uma mensagem enviada por seu pai a Gerrera. Com a promessa de liberdade ao término da missão, ela aceita trabalhar ao lado do capitão Cassian Andor (Diego Luna) e do robô K-2SO.</p>"
+    
+    let img = document.querySelector('#divRO').innerHTML += "<img src='./imagens/epRO.jpg'>"
+}
+
+
+// EP S
+function ocultarS() {
+    let msg = document.querySelector('#divS').innerHTML = "<h3>Solo: A Star Wars Story</h3>"
+}
+function mostrarS() {
+    let msg = document.querySelector('#divS').innerHTML = "<h3>Solo: A Star Wars Story</h3>"
+
+    let msg2 = document.querySelector('#divS').innerHTML += "<p>Em dificuldade financeira, o piloto Han Solo (Alden Ehrenreich) e sua namorada Qi'ra (Emilia Clarke) buscam algum trabalho. Eles conseguem pôr as mãos em uma substância valiosa, mas perigosa, que acaba levando à perseguição do Império e à separação entre os dois. Preocupado, Han usa toda a sua esperteza para se infiltrar num plano arriscado, mas que pode render a oportunidade de voltar à terra natal e reencontrar a sua amada. Caso ele falhe, vai ter que prestar contas ao perigoso Dryden Vos (Paul Bettany). No caminho, o piloto encontra parceiros importantes, como Chewbacca (Joonas Suotamo) e Lando Calrissian (Donald Glover), o proprietário original da Millennium Falcon.</p>"
+    
+    let img = document.querySelector('#divS').innerHTML += "<img src='./imagens/epS.jpg'>"
+}
